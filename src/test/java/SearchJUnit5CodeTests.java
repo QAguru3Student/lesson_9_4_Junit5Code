15a936ff01e85513a31ddad5bb1bfc2bbfbddac1
Babushkin Anton <napishi_antonu@mail.ru>
2021-11-12 14:09:45 +0300

    add searchJUnit5Code test

---
+++ src/test/java/SearchJUnit5CodeTests.java
@@ -0,0 +1,25 @@
+import org.junit.jupiter.api.Test;
+
+import static com.codeborne.selenide.Condition.text;
+import static com.codeborne.selenide.Selectors.byName;
+import static com.codeborne.selenide.Selectors.byText;
+import static com.codeborne.selenide.Selenide.$;
+import static com.codeborne.selenide.Selenide.open;
+
+public class SearchJUnit5CodeTests {
+
+    @Test
+    void searchJUnit5CodeTest(){
+        //Открыть страницу
+        open("https://github.com/");
+        //Перейти в раздел Wiki
+        $(byName("q")).setValue("Selenide").pressEnter();
+        $("ul.repo-list li a").click();
+        $(byText("Wiki")).click();
+        //Проверить наличие страницы и провалиться в нее
+        $("#wiki-pages-filter").setValue("SoftAssertions");
+        $("#wiki-pages-box").$(byText("SoftAssertions")).click();
+        //Проверить наличие кода для JUnit5
+        $(".markdown-body").shouldHave(text("Using JUnit5 extend test class"));
+    }
+}
